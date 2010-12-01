9a99fbb768aa1700bac5883fff8432a505694e99
lolautruche <lolautruche@gmail.com>
2010-12-01 15:33:46 +0100

    Logout module with associated INI file

--- modules/jvlogout/logout.php
+++ modules/jvlogout/logout.php
@@ -12,18 +12,40 @@
 $Module = $Params['Module'];
 $http = eZHTTPTool::instance();
 $Result = array();
+$logoutINI = eZINI::instance( 'jvlogout.ini' );
 
 try
 {
+    $aLogoutHandlers = $logoutINI->variable( 'LogoutSettings', 'AvailableHandlers' );
+    foreach( $aLogoutHandlers as $logoutHandlerClass )
+    {
+        if( !class_exists( $logoutHandlerClass ) )
+        {
+            eZDebug::writeError( "Cannot find logout handler '$logoutHandlerClass'. Did you regenerate autoloads ?" );
+            continue;
+        }
+        
+        $logoutHandler = new $logoutHandlerClass();
+        if( !$logoutHandler instanceof IJVLogoutHandler ) // First check if the handler implements the right interface
+        {
+            eZDebug::writeError( "Declared logout handler '$logoutHandlerClass' does not implement IJVLogoutHandler interface." );
+            unset( $logoutHandler );
+            continue;
+        }
+        
+        $logoutHandler->handleLogout();
+        unset( $logoutHandler );
+    }
     
-    
-    $redirectURI = '/';
-    if ( $http->hasSessionVariable( 'LastAccessesURI' ) )
-        $redirectURI = $http->sessionVariable( 'LastAccessesURI' );
-    $Module->redirectTo( $redirectURI );
 }
 catch( Exception $e )
 {
     $errMsg = $e->getMessage();
     eZDebug::writeError( $errMsg, 'jvLogout' );
 }
+
+// Finally
+$redirectURI = '/';
+if( $logoutINI->hasVariable( 'LogoutSettings', 'LogoutRedirect' ) )
+    $logoutINI->Variable( 'LogoutSettings', 'LogoutRedirect' );
+$Module->redirectTo( $redirectURI );
